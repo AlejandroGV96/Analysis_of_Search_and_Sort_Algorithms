ce1a2626ea901c83e4b3f654bd00c9216ccea2c4
Alejandro <alejandrogarciav96@gmail.com>
2021-05-13 15:17:01 +0200

    transforming DataMatrix for using smart pointers instead of regular pointers

--- Program/src/main.cpp
+++ Program/src/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "chrono.cpp"
 
 enum MatrixInfo
 {
@@ -21,63 +22,88 @@ enum MatrixInfo
 	n4,
 	n5,
 	n6,
-
-	size_n1 = 100,
-	size_n2 = 1000,
-	size_n3 = 10000,
-	size_n4 = 100000,
-	size_n5 = 1000000,
-	size_n6 = 10000000,
-
 };
 
-void matrixValFiller(int ****Matrix,int &usedAlg, int &type,int nArr ,int size ) {
+typedef std::unique_ptr<int[]> sixSizesArrays; // n1 - n6 data sizes
+typedef std::unique_ptr<sixSizesArrays[]> typeOfData; // ordered, reverse and 'random'
+typedef std::unique_ptr<typeOfData[]> algorithms; // algorithms that will sort stored data
+typedef std::unique_ptr<algorithms[]> fullDataStructure; // Data Matrix containing all data
+
+/// <summary>
+///		Function that will fill the data arrays with values depending on its type ( ordered, reversed or 'random')
+/// </summary>
+/// <param name="Matrix"> Data Structure</param>
+/// <param name="alg"> Branch of the data structure (insertion, bubble, merge...) </param>
+/// <param name="type"> Branch of type of data</param>
+/// <param name="nArr"> Brach of data size</param>
+/// <param name="size"> Size available for storage</param>
+void matrixValFiller(fullDataStructure& Matrix, int& alg, int& type, int &nArr, unsigned int& size) {
 	for (int k = 0; k < size; k++) {
 		switch (type) {
 		case ordered:
-			Matrix[usedAlg][type][nArr][k] = k + 1;
+			Matrix[alg][type][nArr][k] = k + 1;
 			break;
 		case reversed:
-			Matrix[usedAlg][type][nArr][k] = size - k;
+			Matrix[alg][type][nArr][k] = size - k;
 			break;
 		case random:
-			Matrix[usedAlg][type][nArr][k] = rand() % size;
+			Matrix[alg][type][nArr][k] = rand() % size;
+			break;
 		}
 	}
 }
 
 int main() {
 
-	// Data Structure for the arrays to be sorted by the algorithms
-	//	DataMatrix[0 - 7] : Set of arrays that will be sorted with the named algorithm (Bubble Sort, Selection Sort, Insertion Sort, Quick Sort, Merge Sort Top-Down, Merge Sort Bottom-Up, Linear Search, Binary Search)
-	//	DataMatrix[][0 - 2] : Set of arrays with different types of data (ordered, reversed, random)
-	//	DataMatrix[][][0-5] : Set of arrays with different sizes of data (n1, n2, n3, n4, n5, n6)
-	int**** DataMatrix = new int***[8]; // First Dimension: 8 Algorithms to use 
-	for (int i = 0; i < 8; i++) {
 
-		std::cout << "Initializing " << i + 1 << "# set of arrays" << std::endl;
-		DataMatrix[i] = new int**[3]; // Second Dimension: 3 types of data for each algorithm
-		
-		// Reseting random seed to ensure that every algorithm will sort the same random array
-		std::srand(1);
+	// possibility to define custom data sizes
+	unsigned int size[6]{100, 1000, 10000, 100000, 1000000, 10000000};
+	//for (auto i = 0; i < 6; i++) {
+	//	std::cin >> size[i];
+	//}
+	{
+		std::cin.get();
 
-		for (int j = 0; j < 3; j++) {
-			DataMatrix[i][j] = new int*[6]; // Third Dimension: 6 sizes of data for each type
+		fullDataStructure DataMatrix = std::make_unique<algorithms[]>(8);
 
-			// Fourth Dimension : Fill the arrays with data depending on required size and data type
-			DataMatrix[i][j][n1] = new int[size_n1];
-				matrixValFiller(DataMatrix, i, j, n1, size_n1);
-			DataMatrix[i][j][n2] = new int[size_n2]{};
-				matrixValFiller(DataMatrix, i, j, n2, size_n2);
-			DataMatrix[i][j][n3] = new int[size_n3]{};
-				matrixValFiller(DataMatrix, i, j, n3, size_n3);
-			DataMatrix[i][j][n4] = new int[size_n4]{};
-				matrixValFiller(DataMatrix, i, j, n4, size_n4);
-			DataMatrix[i][j][n5] = new int[size_n5]{};
-				matrixValFiller(DataMatrix, i, j, n5, size_n5);
-			DataMatrix[i][j][n6] = new int[size_n6]{};
-				matrixValFiller(DataMatrix, i, j, n6, size_n6);
+		for (auto i = 0; i < 8; ++i) {
+			std::cout << "Initializing " << i + 1 << "# set of arrays" << std::endl;
+			Timer timer; // setting benchmarking timer
+			DataMatrix[i] = std::make_unique<typeOfData[]>(3);
+
+			std::srand(1); // reseting randomizer
+
+			for (auto j = 0; j < 3; ++j) {
+				DataMatrix[i][j] = std::make_unique<sixSizesArrays[]>(6);
+
+				for (auto n = 0; n < 6; ++n) {
+					DataMatrix[i][j][n] = std::make_unique<int[]>(size[n]);
+						matrixValFiller(DataMatrix, i, j, n, size[n]);
+				}
+			}
 		}
+
+		std::cout << DataMatrix[insertion][ordered][n6][9000000] << std::endl;
+
+
+
+		std::cin.get();
+
 	}
+	std::cin.get();
+
 
 }
+
+
+/*
+	REFERENCES:
+
+		-Inspiration for using typedef, to create a smart pointers data structure
+		 that allows automatic deallocation of memory used
+		https://stackoverflow.com/questions/15488470/syntax-for-dynamically-allocating-a-2d-array-of-smart-pointers
+
+		-Benchmarking with chrono
+		https://www.youtube.com/watch?v=YG4jexlSAjc
+
+*/
