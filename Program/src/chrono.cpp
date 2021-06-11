e9c5b0fb888de79a39efcdd02f13e1f4e61fe9f6
Alejandro <alejandrogarciav96@gmail.com>
2021-06-11 15:06:25 +0200

    changed a bit to return a string containing the time spent

--- Program/src/chrono.cpp
+++ Program/src/chrono.cpp
@@ -2,19 +2,22 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <iostream>
+#include <string>
 #include "chrono.hpp"
 
 Timer::Timer() : m_StartTimePoint{ std::chrono::high_resolution_clock::now() } {}
 
 
-void Timer::stop() {
+std::string Timer::stop() {
 	auto endTimePoint = std::chrono::high_resolution_clock::now();
 	
 	auto Start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimePoint).time_since_epoch().count();
 	auto End = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();;
 	
 	auto duration = End - Start;
-	auto ms = duration * 0.001;
+	auto ms = std::to_string( duration * 0.001);
+	ms.erase(ms.begin() + ms.find_last_not_of('0'), ms.end());
+	return ms;
 	
-	std::cout << ms << "ms";// << std::endl;
+	//std::cout << ms << "ms";// << std::endl;
 }
