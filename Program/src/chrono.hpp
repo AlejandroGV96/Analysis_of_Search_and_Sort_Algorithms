61e0bf21513a4262fa8a148bc97e32c4cd210e7d
Alejandro <alejandrogarciav96@gmail.com>
2021-06-11 15:06:47 +0200

    change return value of stop()

--- Program/src/chrono.hpp
+++ Program/src/chrono.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <chrono>
+#include <string>
 
 class Timer {
 	std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimePoint;
 public:
 	Timer();
-	void stop();
+	std::string stop();
 };
